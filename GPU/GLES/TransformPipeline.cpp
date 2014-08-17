d2a20bd535c42edc881d8761fb3984c45594e454
Unknown W. Brackets <checkins@unknownbrackets.org>
2014-08-17 14:48:27 -0700

    Mergefix.

--- GPU/GLES/TransformPipeline.cpp
+++ GPU/GLES/TransformPipeline.cpp
@@ -83,6 +83,7 @@
 #include "GPU/Common/VertexDecoderCommon.h"
 #include "GPU/Common/SoftwareTransformCommon.h"
 #include "GPU/GLES/FragmentTestCache.h"
+#include "GPU/GLES/Framebuffer.h"
 #include "GPU/GLES/StateMapping.h"
 #include "GPU/GLES/TextureCache.h"
 #include "GPU/GLES/TransformPipeline.h"
